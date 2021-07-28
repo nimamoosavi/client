9f018280ea9f316d5a6acdd840f5f7413cdaf8a3
nima <nmoosavi@live.com>
2021-07-28 14:44:32 +0430

    add feign client

---
+++ src/main/java/com/webold/client/config/ErrorHandle.java
@@ -0,0 +1,11 @@
+package com.webold.client.config;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class ErrorHandle implements ErrorDecoder {
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        return null;
+    }
+}
